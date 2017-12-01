4a00e1118ecc5955075f71f5b3fbddc55972577c
Jason Lin <jasonlin43212@gmail.com>
2017-11-30 21:47:29 -0500

    created and wrote the SuperArrayIterator interface

---
+++ 06SuperArrayIterator/SuperArrayIterator.java
@@ -0,0 +1,19 @@
+public interface SuperArrayIterator implements Iterator<String>{
+  private SuperArray data;
+  private currentIndex;
+
+  public SuperArrayIterator(SuperArray a) {
+    data = a;
+    currentIndex = 0;
+  }
+
+  public boolean hasNext(){
+    return currentIndex < data.size();
+  }
+
+  public String next(){
+    String result = data.get(currentIndex);
+    currentIndex++;
+    return result;
+  }
+}
