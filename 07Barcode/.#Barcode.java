9e594d6b6b1387459dfa011d7a21c6f5e5e8a184
Jason Lin <jasonlin43212@gmail.com>
2017-12-02 20:50:24 -0500

    pushed saved file

--- 07Barcode/.#Barcode.java
+++
@@ -1,1 +0,0 @@
-kachi@kachi-Lenovo-ideapad-720S-14IKB.32119:1512232076
--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -17,6 +17,21 @@ public class Barcode{
     return sum % 10;
   }
 
+  public String getCode(){
+    String num = zip + getCheckSum();
+    String output = "|";
+    for (int i=0; i<num.length();i++){
+      output += convert(num.substring(i,i+1));
+    }
+    output += "|";
+    return output;
+  }
+
+  private String convert(String str){
+    String[] conversion = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
+    return conversion[Integer.parseInt(str)];
+  }
+
   public String toString(){
     return getCode() + " (" + getZip() + ")";
   }
