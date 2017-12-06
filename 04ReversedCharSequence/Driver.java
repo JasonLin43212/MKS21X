b54ead726cac0dd4c7f59b51f4224f8d5e9f09ef
Jason Lin <jasonlin43212@gmail.com>
2017-12-05 22:08:36 -0500

    changed name to ReversedCharSequence instead of ReversedCharSeq

--- 04ReversedCharSequence/Driver.java
+++ 04ReversedCharSequence/Driver.java
@@ -1,12 +1,16 @@
 
 public class Driver{
   public static void main(String[] args){
-    ReversedCharSequence test = new ReversedCharSequence("ABCD");
-    System.out.println(test);
-    System.out.println(test.charAt(0));
-    //System.out.println(test.charAt(-1));
-    System.out.println(test.charAt(4));
-    System.out.println(test.length());
-    System.out.println(test.subSequence(0,3));
-}
+    ReversedCharSequence a = new ReversedCharSequence("Hello");
+    ReversedCharSequence b = new ReversedCharSequence(a);
+
+    System.out.println(a);//olleH
+    System.out.println(b);//Hello
+    System.out.println(a.charAt(3));//e
+    System.out.println(a.length());//5
+    System.out.println(a.toString());//olleH
+    System.out.println(a.subSequence(2,4));//le
+    System.out.println(a.subSequence(0,3));//oll
+    System.out.println(a);//olleH
+  }
 }
