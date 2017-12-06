61acd089118e2ebfc7fc48e62cc1294f91cc1f93
Jason Lin <jasonlin43212@gmail.com>
2017-12-05 20:39:29 -0500

    made more test cases

--- 08Sorts/Driver.java
+++ 08Sorts/Driver.java
@@ -1,13 +1,35 @@
 public class Driver{
   public static void main(String[]args){
-    int[] a = {1,2,34,4,54,9,34,992,44};
+    //Change this part with any test case you like by
+    //commenting and uncommenting
+
+    int[] a = {64, 25, 12, 22, 11};
+    //int[] a = {34, 25, 12, 22, 12};
+    //int[] a = {6, 25, -12, 22, 1, 32, 22};
+    //int[] a = {-5, -25, -12, -15, 1};
+    //int[] a = {1, 1, 0, 1, 0, 0};
+    //int[] a = {1932, -25, 12, 12, 3};
+    //int[] a = {4, 23, -12, 0, 11, 1, 4, 99, 32, 14, 19, 23};
+
+    String input = "[";
     for (int i=0;i<a.length;i++){
-      System.out.println(a[i]);
+      input += a[i];
+      if (i!=a.length-1){
+        input += ",";
+      }
     }
+    input += "]";
+    System.out.println(input);
     Sorts.selectionSort(a);
-    System.out.println("-----Sorted-----");
+    System.out.println("-----Sorted Below----");
+    String output = "[";
     for (int i=0;i<a.length;i++){
-      System.out.println(a[i]);
+      output += a[i];
+      if (i!=a.length-1){
+        output += ",";
+      }
     }
+    output += "]";
+    System.out.println(output);
   }
 }
